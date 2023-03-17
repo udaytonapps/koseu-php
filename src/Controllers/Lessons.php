9cfd2707c11d0da0dbd38984957b4bfd904bd66a
Andrew Niekamp <aniekamp1@udayton.edu>
2023-03-17 16:57:58 -0400

    TSUGI-512 Update lti structure with option for external store

--- src/Controllers/Lessons.php
+++ src/Controllers/Lessons.php
@@ -9,23 +9,33 @@ use Tsugi\Lumen\Application;
 use Illuminate\Http\Request;
 use Tsugi\UI\LessonsOrchestrator;
 
-class Lessons {
+class Lessons
+{
 
-    const ROUTE = '/categories';
+    const ROUTE = '/programs';
 
     const REDIRECT = 'koseu_controllers_lessons';
 
-    public static function routes(Application $app, $prefix=self::ROUTE) {
-        $app->router->get($prefix. '/{category}' . $prefix . '_launch/{anchor}', function(Request $request, $category = null, $anchor = null) use ($app) {
+    public static function routes(Application $app, $prefix = self::ROUTE)
+    {
+        $app->router->get($prefix . '/{program}/{module}/lti-launch/{ltiAnchor}', function (Request $request, $program = null, $module = null, $ltiAnchor = null) use ($app) {
             $redirectUrl = $request->query('redirect_url', '/');
             $autoRegisterId = $request->query('auto_register_id');
             if (isset($autoRegisterId)) {
                 $_SESSION["auto_register_id"] = $autoRegisterId;
             }
-            return Lessons::launch($app, $category, $anchor, $redirectUrl);
+            return Lessons::launch($app, $program, $module, null, $ltiAnchor, $redirectUrl);
         });
-        $app->router->get($prefix . '/{category}/{module}', 'Lessons@get');
-        $app->router->get($prefix . '/{category}/{module}/{page}', 'Lessons@get');
+        $app->router->get($prefix . '/{program}/{module}/{page}/lti-launch/{ltiAnchor}', function (Request $request, $program = null, $module = null, $page = null, $ltiAnchor = null) use ($app) {
+            $redirectUrl = $request->query('redirect_url', '/');
+            // $autoRegisterId = $request->query('auto_register_id');
+            // if (isset($autoRegisterId)) {
+            //     $_SESSION["auto_register_id"] = $autoRegisterId;
+            // }
+            return Lessons::launch($app, $program, $module, $page, $ltiAnchor, $redirectUrl);
+        });
+        $app->router->get($prefix . '/{program}/{module}', 'Lessons@get');
+        $app->router->get($prefix . '/{program}/{module}/{page}', 'Lessons@get');
         // $app->router->get($prefix . '/{course}/{??module??}/badges', 'Lessons@get');
         // /badges .get()
         // $app->router->get($prefix . '/{course}/{??module??}/discussions', 'Lessons@get');
@@ -33,23 +43,23 @@ class Lessons {
         // $app->router->get($prefix . '/{course}/{??module??}/progress', 'Lessons@get');
         // /progress .get()
 
-        $app->router->get($prefix . '/{category}', 'Lessons@get');
+        $app->router->get($prefix . '/{program}', 'Lessons@get');
         $app->router->get($prefix, 'Lessons@get');
-        $app->router->get($prefix.'/', 'Lessons@get');
-        $app->router->get('/'.self::REDIRECT, 'Lessons@get');
+        $app->router->get($prefix . '/', 'Lessons@get');
+        $app->router->get('/' . self::REDIRECT, 'Lessons@get');
     }
 
-    public static function get(Request $request, $anchor = null, $category = null, $module = null, $page = null)
+    public static function get(Request $request, $anchor = null, $program = null, $module = null, $page = null)
     {
         global $CFG, $OUTPUT;
 
-        if ( ! isset($CFG->lessons) ) {
+        if (!isset($CFG->lessons)) {
             die_with_error_log('Cannot find lessons.json ($CFG->lessons)');
         }
 
         // Turning on and off styling
-        if ( isset($_GET['nostyle']) ) {
-            if ( $_GET['nostyle'] == 'yes' ) {
+        if (isset($_GET['nostyle'])) {
+            if ($_GET['nostyle'] == 'yes') {
                 $_SESSION['nostyle'] = 'yes';
             } else {
                 unset($_SESSION['nostyle']);
@@ -61,61 +71,67 @@ class Lessons {
         $_SESSION["login_return"] = $path->full;
 
         // Load the Lesson
-        $l = \Tsugi\UI\LessonsOrchestrator::getLessons($category, $module, $page);
+        $l = \Tsugi\UI\LessonsOrchestrator::getLessons($program, $module, $page);
 
         $OUTPUT->header();
         $OUTPUT->bodyStart();
-        if (file_exists($CFG->dirroot.'/../nav.php')) {
-            include $CFG->dirroot.'/../nav.php';
+        if (file_exists($CFG->dirroot . '/../nav.php')) {
+            include $CFG->dirroot . '/../nav.php';
         }
         $OUTPUT->topNav();
         $OUTPUT->flashMessages();
         $l->header();
-        echo('<div class="">');
+        echo ('<div class="">');
         $l->render();
-        echo('</div>');
+        echo ('</div>');
         $OUTPUT->footerStart();
         $l->footer();
         $OUTPUT->footerEnd();
     }
 
-    public static function launch(Application $app, $category = null, $anchor=null, $redirectUrl)
+    public static function launch(Application $app, $programAnchor = null, $moduleAnchor = null, $pageAnchor = null, $ltiAnchor = null, $redirectUrl)
     {
         global $CFG;
-        $tsugi = $app['tsugi'];
+        // $tsugi = $app['tsugi'];
 
         $path = U::rest_path();
         $redirect_path = U::addSession($path->parent);
-        if ( $redirect_path == '') $redirect_path = '/';
+        if ($redirect_path == '') $redirect_path = '/';
 
-        if ( ! isset($CFG->lessons) ) {
+        if (!isset($CFG->lessons)) {
             $app->tsugiFlashError(__('Cannot find lessons.json ($CFG->lessons)'));
             return redirect($redirect_path);
         }
 
         /// Load the Lesson
-        $l = \Tsugi\UI\LessonsOrchestrator::getLessons($category);
-        if ( ! $l ) {
+        $l = \Tsugi\UI\LessonsOrchestrator::getLessons($programAnchor);
+        if (!$l) {
             $app->tsugiFlashError(__('Cannot load lessons.'));
             return redirect($redirect_path);
         }
 
-        $module = $l->getModuleByRlid($anchor);
-        if ( ! $module ) {
+        $module = $l->getModuleByRlid($ltiAnchor);
+        if (!$module) {
             $app->tsugiFlashError(__('Cannot find module resource link id'));
             return redirect($redirect_path);
         }
 
-        $lti = $l->getLtiByRlid($anchor);
-        if ( ! $lti ) {
+        $lti = $l->getLtiByRlid($ltiAnchor);
+        if (!$lti) {
             $app->tsugiFlashError(__('Cannot find lti resource link id'));
             return redirect($redirect_path);
         }
 
         // Check that the session has the minimums...
-        if ( U::get($_SESSION,'secret') && U::get($_SESSION,'context_key')
-                && U::get($_SESSION,'user_key') && U::get($_SESSION,'displayname') && U::get($_SESSION,'email') )
-        {
+
+        if (
+            isset($lti)
+            && U::get($_SESSION, 'secret')
+            && $l && $module
+            && U::get($_SESSION, 'user_key')
+            && U::get($_SESSION, 'displayname')
+            && U::get($_SESSION, 'email')
+        ) {
             // All good
         } else {
             // Missing session data so redirect to login
@@ -127,7 +143,7 @@ class Lessons {
         $resource_link_title = isset($lti->title) ? $lti->title : $module->title;
         $key = isset($_SESSION['oauth_consumer_key']) ? $_SESSION['oauth_consumer_key'] : false;
         $secret = false;
-        if ( isset($_SESSION['secret']) ) {
+        if (isset($_SESSION['secret'])) {
             $secret = LTIX::decrypt_secret($_SESSION['secret']);
         }
 
@@ -138,39 +154,51 @@ class Lessons {
             'resource_link_title' => $resource_link_title,
             'tool_consumer_info_product_family_code' => 'tsugi',
             'tool_consumer_info_version' => '1.1',
-            'context_id' => $_SESSION['context_key'],
-            // 'context_label' => $CFG->context_title,
-            // 'context_title' => $CFG->context_title,
+            'context_id' => "{$programAnchor}_{$moduleAnchor}",
+            'context_label' => $module->title,
+            'context_title' => $module->title,
             'user_id' => $_SESSION['user_key'],
             'lis_person_name_full' => $_SESSION['displayname'],
             'lis_person_contact_email_primary' => $_SESSION['email'],
             'roles' => isset($_SESSION["admin"]) ? 'Instructor' : 'Learner',
         );
-        if ( isset($_SESSION['avatar']) ) $parms['user_image'] = $_SESSION['avatar'];
+        if (isset($_SESSION['avatar'])) $parms['user_image'] = $_SESSION['avatar'];
 
-        if ( isset($lti->custom) ) {
-            foreach($lti->custom as $custom) {
-                if ( isset($custom->value) ) {
-                    $parms['custom_'.$custom->key] = $custom->value;
+        if (isset($lti->custom)) {
+            foreach ($lti->custom as $custom) {
+                if (isset($custom->value)) {
+                    $parms['custom_' . $custom->key] = $custom->value;
                 }
-                if ( isset($custom->json) ) {
-                    $parms['custom_'.$custom->key] = json_encode($custom->json);
+                if (isset($custom->json)) {
+                    $parms['custom_' . $custom->key] = json_encode($custom->json);
                 }
             }
         }
 
-        $return_url = $path->parent . '/' . str_replace('_launch', '', $path->controller) . '/' . $module->anchor;
+        // Might need for some cases?
+        // $return_url = $path->parent . '/' . str_replace('_launch', '', $path->controller) . '/' . $module->anchor;
+
+        $return_url = "{$CFG->apphome}/programs/{$programAnchor}/{$moduleAnchor}/{$pageAnchor}";
+
         $parms['launch_presentation_return_url'] = $return_url;
 
-        $sess_key = 'tsugi_top_nav_'.$CFG->wwwroot;
-        if ( isset($_SESSION[$sess_key]) ) {
+        $sess_key = 'tsugi_top_nav_' . $CFG->wwwroot;
+        if (isset($_SESSION[$sess_key])) {
             // $parms['ext_tsugi_top_nav'] = $_SESSION[$sess_key];
         }
 
-        $form_id = "tsugi_form_id_".bin2Hex(openssl_random_pseudo_bytes(4));
+        $form_id = "tsugi_form_id_" . bin2Hex(openssl_random_pseudo_bytes(4));
         $parms['ext_lti_form_id'] = $form_id;
 
-        $endpoint = $lti->launch;
+        if (isset($lti->external_launch)) {
+            $endpoint = $lti->external_launch;
+            // $key = "12345";
+            // $secret = "secret";
+            $key = $CFG->external_store_key;
+            $secret = $CFG->external_store_secret;
+        } else {
+            $endpoint = $lti->launch;
+        }
 
         if (isset($redirectUrl) && strlen($redirectUrl) > 0) {
             $parms['redirect_url'] = $redirectUrl;
@@ -183,17 +211,24 @@ class Lessons {
             try {
                 LessonsOrchestrator::autoRegisterAttendee($regKey);
                 $_SESSION["auto_register_id"] = null;
-            } catch(\Exception $e) {
+            } catch (\Exception $e) {
                 return $e->getMessage();
             }
         }
-        
-        $parms = LTI::signParameters($parms, $endpoint, "POST", $key, $secret,
-            "Finish Launch", $CFG->wwwroot, $CFG->servicename);
+
+        $parms = LTI::signParameters(
+            $parms,
+            $endpoint,
+            "POST",
+            $key,
+            $secret,
+            "Finish Launch",
+            $CFG->wwwroot,
+            $CFG->servicename
+        );
 
         $content = LTI::postLaunchHTML($parms, $endpoint, false /*debug */);
         print($content);
         return "";
     }
-
 }
