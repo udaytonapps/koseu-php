39e28466d63e8bafc27d65c18d0a64c0228ac7a9
Andrew Niekamp <aniekamp1@udayton.edu>
2023-03-21 17:11:31 -0400

    TSUGI-514 Add progress template and update module progress

--- src/Controllers/Assignments.php
+++ src/Controllers/Assignments.php
@@ -6,22 +6,26 @@ use Tsugi\Lumen\Application;
 use Symfony\Component\HttpFoundation\Request;
 
 use \Tsugi\Grades\GradeUtil;
+use Tsugi\UI\LessonsOrchestrator;
+use Tsugi\UI\LessonsUIHelper;
 use Tsugi\Util\U;
 
-class Assignments {
+class Assignments
+{
 
     const ROUTE = '/progress';
 
-    public static function routes(Application $app, $prefix=self::ROUTE) {
+    public static function routes(Application $app, $prefix = self::ROUTE)
+    {
         $app->router->get($prefix, 'Assignments@get');
-        $app->router->get($prefix.'/', 'Assignments@get');
+        $app->router->get($prefix . '/', 'Assignments@get');
     }
 
     public function get(Request $request)
     {
         global $CFG, $OUTPUT;
 
-        if ( ! isset($CFG->lessons) ) {
+        if (!isset($CFG->lessons)) {
             die_with_error_log('Cannot find lessons.json ($CFG->lessons)');
         }
 
@@ -29,15 +33,25 @@ class Assignments {
         $path = U::rest_path();
         $_SESSION["login_return"] = $path->full;
 
-        // Load the Lesson
-        $l = \Tsugi\UI\LessonsOrchestrator::getLessons(); // TODO
+        // Load all Lessons
+        $referenceList = \Tsugi\UI\LessonsOrchestrator::getLessonsReference();
+
+        /** @var CourseBase[] */
+        $lessonsList = [];
+        $moduleData = [];
+        foreach ($referenceList as $key => $reference) {
+            $l = \Tsugi\UI\LessonsOrchestrator::getLessons($key);
+            array_push($lessonsList, $l);
+            $response = $l->getProgressData($key, $l);
+            $moduleData[] = $response;
+        }
 
         // Load all the Grades so far
         $allgrades = array();
         $alldates = array();
-        if ( isset($_SESSION['id']) && isset($_SESSION['context_id'])) {
+        if (isset($_SESSION['id']) && isset($_SESSION['context_id'])) {
             $rows = GradeUtil::loadGradesForCourse($_SESSION['id'], $_SESSION['context_id']);
-            foreach($rows as $row) {
+            foreach ($rows as $row) {
                 $allgrades[$row['resource_link_id']] = $row['grade'];
                 $alldates[$row['resource_link_id']] = $row['updated_at'];
             }
@@ -45,12 +59,32 @@ class Assignments {
 
         $OUTPUT->header();
         $OUTPUT->bodyStart();
-        if (file_exists($CFG->dirroot.'/../nav.php')) {
-            include $CFG->dirroot.'/../nav.php';
+        if (file_exists($CFG->dirroot . '/../nav.php')) {
+            include $CFG->dirroot . '/../nav.php';
         }
         $OUTPUT->topNav();
         $OUTPUT->flashMessages();
         $l->renderAssignments($allgrades, $alldates, false);
+
+        $twig = LessonsUIHelper::twig();
+
+        $twig->addFunction(new \Twig\TwigFunction('getModuleByRlid', function ($course, $resource_link_id) {
+            return LessonsOrchestrator::getModuleByRlid($course, $resource_link_id);
+        }));
+        $twig->addFunction(new \Twig\TwigFunction('getLtiByRlid', function ($course, $resource_link_id) {
+            return LessonsOrchestrator::getLtiByRlid($course, $resource_link_id);
+        }));
+        $twig->addFunction(new \Twig\TwigFunction('getGradeByRlid', function ($grades, $resource_link_id) {
+            if (isset($grades) && isset($grades[$resource_link_id])) {
+                return $grades[$resource_link_id];
+            }
+        }));
+
+        echo $twig->render('progress.twig', [
+            'modules' => $moduleData,
+            'loggedIn' => isset($_SESSION['id'])
+        ]);
+
         $OUTPUT->footer();
     }
 }
